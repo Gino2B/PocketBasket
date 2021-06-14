efd6850ccdfc54c4102f281f833e1fd36fb47d1d
Gino2B <gbaybay30@gmail.com>
2021-06-14 12:02:07 -0400

    Fixed hoisting issues

--- index.js
+++ index.js
@@ -69,6 +69,59 @@ const playAgain = () => {
   });
 };
 
+//searches for player by full name and gets their Full name back and their player ID
+const searchPlayer = async (playerName) => {
+    try{
+    clearUI(); 
+    const response = await axios.get(`${API}/players?search=${playerName}`);
+    const responseData = response.data.data[0];
+    const firstName = responseData.first_name;
+    const lastName = responseData.last_name;
+    const fullName = `${firstName} ${lastName}`;
+    const pictureResponse = await axios.get(`${pictureAPI}players/${lastName}/${firstName}`);
+
+    //set up the select image functionality
+    const searchImgPicture = document.createElement('img');
+    const searchImageATag = document.createElement('a'); //creating an anchor link to jump to the gameplay portion of the page
+    searchImgPicture.src = pictureResponse.config.url;
+    searchImageATag.href = "#play-game"
+    const selectText = document.createElement('p');
+
+    //clicking on the image will select the player and show the rest of the UI 
+    selectText.innerText = "Click on the image to select this player";
+    searchImageATag.appendChild(searchImgPicture);
+    searchImageDiv.appendChild(searchImageATag);
+    searchImageDiv.appendChild(selectText);
+    searchImgPicture.addEventListener('click',()=>{
+      actionBox.style.visibility = "visible";
+      playerUI.style.visibility = "visible";
+      computerUI.style.visibility = "visible";
+      outerContainer.style.visibility = "visible";
+    });
+
+    //get data for the player UI box
+    const displayName = document.createElement('p');
+    displayName.innerText = fullName;
+    const position = responseData.position;
+    const displayPosition = document.createElement('p');
+    displayPosition.innerText = position;
+
+
+    //loads the player UI box onto the page
+    const img = document.createElement('img');
+    img.src = pictureResponse.config.url;
+    playerImageContainer.appendChild(img);
+    playerUIContainer.append(displayName,displayPosition);
+    
+    const playerID = responseData.id;
+    seasonAverages(playerID, fullName);
+ 
+
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
 //Uses playerID to get season averages and data
 const seasonAverages = async (playerID, playerName) => {
   try{  
@@ -124,8 +177,7 @@ const seasonAverages = async (playerID, playerName) => {
           const damage = Math.floor((baseDamageShoot + Math.floor((threePointPerc*threePointMade)))); //standard damage
           computerHPvalue -= damage; //updates the user health
           computerHP.innerText = computerHPvalue;//sets the users health for screen use
-  
-          
+
           //checks for winner 
           checkWinner();
           //setting up the move log history
@@ -162,7 +214,6 @@ const seasonAverages = async (playerID, playerName) => {
           if(gameover === false){
             setTimeout(function(){ actionBox.style.visibility = "visible"; }, threeSeconds);
           }
-  
         }else {
           const damage = Math.floor((baseDamageSteal) + Math.floor((steals+blocks))); //standard damage
           computerHPvalue -= damage; //updates the user health
@@ -266,60 +317,40 @@ const seasonAverages = async (playerID, playerName) => {
   }
 };
 
+//hardcoding options for computer to select from the top 20 players (IMO)
+const playerNames = ["Lebron James", "Kevin Durant", "James Harden", "Anthony Davis", "Giannis Antetokounmpo","Stephen Curry", "Kawhi Leonard", "Jimmy Butler", "Joel Embiid","Nikola Jokic", "Damian Lillard", "Bradley Beal", "Jayson Tatum", "Russell Westbrook", "Kyrie Irving", "Paul George", "Chris Paul", "Devin Booker", "Donovan Mitchell", "Karl-Anthony Towns" ]
 
-//searches for player by full name and gets their Full name back and their player ID
-const searchPlayer = async (playerName) => {
-    try{
-    clearUI(); 
-    const response = await axios.get(`${API}/players?search=${playerName}`);
+
+const computerPlayer = async (computerName) => {
+  try{
+    const response = await axios.get(`${API}/players?search=${computerName}`);
     const responseData = response.data.data[0];
     const firstName = responseData.first_name;
     const lastName = responseData.last_name;
     const fullName = `${firstName} ${lastName}`;
     const pictureResponse = await axios.get(`${pictureAPI}players/${lastName}/${firstName}`);
 
-    //set up the select image functionality
-    const searchImgPicture = document.createElement('img');
-    const searchImageATag = document.createElement('a'); //creating an anchor link to jump to the gameplay portion of the page
-    searchImgPicture.src = pictureResponse.config.url;
-    searchImageATag.href = "#play-game"
-    const selectText = document.createElement('p');
-
-    //clicking on the image will select the player and show the rest of the UI 
-    selectText.innerText = "Click on the image to select this player";
-    searchImageATag.appendChild(searchImgPicture);
-    searchImageDiv.appendChild(searchImageATag);
-    searchImageDiv.appendChild(selectText);
-    searchImgPicture.addEventListener('click',()=>{
-      actionBox.style.visibility = "visible";
-      playerUI.style.visibility = "visible";
-      computerUI.style.visibility = "visible";
-      outerContainer.style.visibility = "visible";
-    });
-
-    //get data for the player UI box
+    //get data for the computer UI box
     const displayName = document.createElement('p');
     displayName.innerText = fullName;
     const position = responseData.position;
     const displayPosition = document.createElement('p');
     displayPosition.innerText = position;
 
-
     //loads the player UI box onto the page
     const img = document.createElement('img');
     img.src = pictureResponse.config.url;
-    playerImageContainer.appendChild(img);
-    playerUIContainer.append(displayName,displayPosition);
+    computerImageContainer.appendChild(img);
+    computerUIContainer.append(displayName,displayPosition);
     
     const playerID = responseData.id;
-    seasonAverages(playerID, fullName);
+    computerSeasonAverages(playerID, fullName);
  
 
   } catch (error) {
     console.error(error.message);
   }
 };
-
 
 //repeat the same code above for the computer player. Make it so that after you click on any of the moves buttons it will then make the computer move like 1 second later
 
@@ -539,47 +570,8 @@ const computerSeasonAverages = async (playerID, playerName) => {
   } 
 };
 
-
-
-//hardcoding options for computer to select from the top 20 players (IMO)
-const playerNames = ["Lebron James", "Kevin Durant", "James Harden", "Anthony Davis", "Giannis Antetokounmpo","Stephen Curry", "Kawhi Leonard", "Jimmy Butler", "Joel Embiid","Nikola Jokic", "Damian Lillard", "Bradley Beal", "Jayson Tatum", "Russell Westbrook", "Kyrie Irving", "Paul George", "Chris Paul", "Devin Booker", "Donovan Mitchell", "Karl-Anthony Towns" ]
-
-
-const computerPlayer = async (computerName) => {
-  try{
-    const response = await axios.get(`${API}/players?search=${computerName}`);
-    const responseData = response.data.data[0];
-    const firstName = responseData.first_name;
-    const lastName = responseData.last_name;
-    const fullName = `${firstName} ${lastName}`;
-    const pictureResponse = await axios.get(`${pictureAPI}players/${lastName}/${firstName}`);
-
-    //get data for the computer UI box
-    const displayName = document.createElement('p');
-    displayName.innerText = fullName;
-    const position = responseData.position;
-    const displayPosition = document.createElement('p');
-    displayPosition.innerText = position;
-
-    //loads the player UI box onto the page
-    const img = document.createElement('img');
-    img.src = pictureResponse.config.url;
-    computerImageContainer.appendChild(img);
-    computerUIContainer.append(displayName,displayPosition);
-    
-    const playerID = responseData.id;
-    computerSeasonAverages(playerID, fullName);
- 
-
-  } catch (error) {
-    console.error(error.message);
-  }
-};
-
-
 const playerForm = document.querySelector('form')
 const playerSearch = document.querySelector('#player-search')
-
 
 playerForm.addEventListener('submit', async (e) => {
   e.preventDefault();
